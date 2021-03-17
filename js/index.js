bd66c5bb52e7e06b341c6e9b2397f853b96cd6fd
Julien Leblanc <julien.leblanc@epfedu.fr>
2021-03-17 20:05:36 +0100

    lab done

--- js/index.js
+++ js/index.js
@@ -28,7 +28,6 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 memoryGame.shuffleCards();
 
-
 window.addEventListener('load', event => {
   let html = '';
   memoryGame.cards.forEach(pic => {
@@ -44,7 +43,37 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      // Ajout carte dans tableau pickedcard en fonction de sa longeur (2 cartes max)
+      if (memoryGame.pickedCards.length <= 1){
+        memoryGame.pickedCards.push(card);
+        card.classList.toggle("turned");
+        //Si 2 cartes
+        if (memoryGame.pickedCards.length === 2){          
+          let pickedCardName1 = memoryGame.pickedCards[0].getAttribute("data-card-name");
+          let pickedCardName2 = memoryGame.pickedCards[1].getAttribute("data-card-name");
+          //Vérif si identitique
+          if (memoryGame.checkIfPair(pickedCardName1,pickedCardName2)){
+              document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;// incrémentation du nbr de tentatives
+              document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed; // incrémentation du score
+              //Verif si jeu terminé
+              if(memoryGame.isFinished()){
+                console.log("gagné");
+              }             
+          }
+          else {
+            document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;// incrémentation du nbr de tentatives
+            //function timeOut pour laisser le tps au joueur de mémoriser
+            let myArr = memoryGame.pickedCards;
+            setTimeout(()=>{
+              myArr.forEach(pickedCard => {
+              //Cartes se retournent de nouveau
+              pickedCard.classList.toggle("turned");
+              })
+            },1000)
+          }
+          memoryGame.pickedCards = [];
+        }
+      }      
       console.log(`Card clicked: ${card}`);
     });
   });
--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,5 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
     this.pairsClicked = 0;
@@ -7,35 +7,24 @@ class MemoryGame {
     // add the rest of the class properties here
   }
   shuffleCards() {
-    if (!this.cards){
+    if (!this.cards) {
       return undefined;
     }
-    // if (this.cards.length === 0) {
-    //   return undefined;
-    // }
-      // -- To shuffle an array a of n elements (indices 0..n-1):
-      // for i from n−1 downto 1 do
-      //      j ← random integer such that 0 ≤ j ≤ i
-      //      exchange a[j] and a[i]
-      //       function entierAleatoire(min, max)
-      // {
-      //  return Math.floor(Math.random() * (max - min + 1)) + min;
-      // }
 
-      for (let i = 0; i < this.cards.length;i++){
-        let rand = Math.trunc(Math.random() * (i + 1));
-        let shuffledValue = this.cards[i];
-        this.cards[i] = this.cards[rand];
-        this.cards[rand] = shuffledValue;
-      }
-      return this.cards;
+    for (let i = 0; i < this.cards.length; i++) {
+      let rand = Math.trunc(Math.random() * (i + 1));
+      let shuffledValue = this.cards[i];
+      this.cards[i] = this.cards[rand];
+      this.cards[rand] = shuffledValue;
+    }
+    return this.cards;
   }
   checkIfPair(card1, card2) {
     this.pairsClicked++;
-    if (card1 === card2){
+    if (card1 === card2) {
       this.pairsGuessed++;
       return true;
-    }else{
+    } else {
       return false;
     }
   }
